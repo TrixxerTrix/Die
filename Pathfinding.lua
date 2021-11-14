57ddfba33ad041f1fc48254b0703fb55ddc335a0
Trix <78498219+TrixxerTrix@users.noreply.github.com>
2021-11-14 16:37:48 +0000

    Update and rename die.lua to Pathfinding.lua

--- Pathfinding.lua
+++ Pathfinding.lua
@@ -1,8 +1,8 @@
 local pfs,plrs = game:GetService("PathfindingService"),game:GetService("Players")
 local http,rs = game:GetService("HttpService"),game:GetService("ReplicatedStorage")
 local hrp = plrs.LocalPlayer.Character.HumanoidRootPart
-function pathFind(pos)
-    if not typeof(pos) == "Vector3" then return end
+function _G.pathFind(pos)
+    if not typeof(pos) == ("Vector3") then return end
     local path = pfs:CreatePath({});path:ComputeAsync(hrp.Position,pos)
     local pathfinding_fol = Instance.new("Folder",workspace)
     pathfinding_fol.Name = http:GenerateGUID(true)
@@ -21,7 +21,7 @@ function pathFind(pos)
     local wps = path:GetWaypoints()
     for i, wp in next, wps do
         local part = Instance.new("Part")
-        part.Shape,part.Material,part.Size = "Ball",Enum.Material.Neon,Vector3.new(0.6, 0.6, 0.6)
+        part.Shape,part.Material,part.Size = "Block",Enum.Material.Neon,Vector3.new(0.6, 0.6, 0.6)
         part.Position,part.Anchored,part.CanCollide = wp.Position + Vector3.new(0,1.6,0),true,false
         part.Parent,part.Transparency = pathfinding_fol,0.6
         if i == 1 then
@@ -46,20 +46,8 @@ function pathFind(pos)
     end
     return
 end
-function pathFindTable(table)
+function _G.pathFindTable(table)
     if not typeof(table) == "table" then return end
     for i, v3 in next, table do pathFind(v3) end
     return
 end
-for i = 1,2 do
-    pathFindTable({
-        Vector3.new(-1011.2380981445, 17.99347114563, 8957.63671875),
-        Vector3.new(-1009.1943359375, 17.99347114563, 8992.517578125),
-        Vector3.new(-959.66491699219, 17.99347114563, 8991.5546875),
-        Vector3.new(-958.37261962891, 17.99347114563, 8957.392578125)
-    })
-end
-pathFindTable({
-    Vector3.new(-1031.4885253906, 26.392995834351, 8985.8662109375),
-    Vector3.new(-989.02899169922, 26.393482208252, 9015.9345703125)
-})
