694e12393d98e9a19e64e7a1040e8ee8507f2183
Trix <78498219+TrixxerTrix@users.noreply.github.com>
2021-11-14 14:41:16 +0000

    Update die.lua

--- die.lua
+++ die.lua
@@ -1,11 +1,13 @@
 local pfs,plrs = game:GetService("PathfindingService"),game:GetService("Players")
+local http,rs = game:GetService("HttpService"),game:GetService("ReplicatedStorage")
+local hrp = plrs.LocalPlayer.Character.HumanoidRootPart
 function pathFind(pos)
-    if not typeof(pos) == "Vector3" then return end
-    local path = pfs:CreatePath({});path:ComputeAsync(plrs.LocalPlayer.Character.HumanoidRootPart.Position,pos)
+    if not typeof(pos) == ("Vector3") then return end
+    local path = pfs:CreatePath({});path:ComputeAsync(hrp.Position,pos)
     local pathfinding_fol = Instance.new("Folder",workspace)
-    pathfinding_fol.Name = game:GetService("HttpService"):GenerateGUID(true)
-    local antiseatfol = Instance.new("Folder",game:GetService("ReplicatedStorage"))
-    antiseatfol.Name = game:GetService("HttpService"):GenerateGUID(true)
+    pathfinding_fol.Name = http:GenerateGUID(true)
+    local antiseatfol = Instance.new("Folder",rs)
+    antiseatfol.Name = http:GenerateGUID(true)
     spawn(function()
         if not _G.SeatsAvoided then
             for i, v in next, workspace:GetDescendants() do
@@ -39,17 +41,25 @@ function pathFind(pos)
     end
     pathfinding_fol:Destroy()
     antiseatfol.Parent = workspace
-    plrs.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(pos)
     for i, v in next, workspace:GetChildren() do
         if v:IsA("Folder") and #v:GetChildren() == 0 then v:Destroy() end
     end
     return
 end
-for i = 1,2 do
-    pathFind(Vector3.new(-1011.2380981445, 17.99347114563, 8957.63671875))
-    pathFind(Vector3.new(-1009.1943359375, 17.99347114563, 8992.517578125))
-    pathFind(Vector3.new(-959.66491699219, 17.99347114563, 8991.5546875))
-    pathFind(Vector3.new(-958.37261962891, 17.99347114563, 8957.392578125))
+function pathFindTable(table)
+    if not typeof(table) == "table" then return end
+    for i, v3 in next, table do pathFind(v3) end
+    return
 end
-pathFind(Vector3.new(-1031.4885253906, 26.392995834351, 8985.8662109375))
-pathFind(Vector3.new(-989.02899169922, 26.393482208252, 9015.9345703125))
+for i = 1,2 do
+    pathfindTable({
+        [1] = Vector3.new(-1011.2380981445, 17.99347114563, 8957.63671875),
+        [2] = Vector3.new(-1009.1943359375, 17.99347114563, 8992.517578125),
+        [3] = Vector3.new(-959.66491699219, 17.99347114563, 8991.5546875),
+        [4] = Vector3.new(-958.37261962891, 17.99347114563, 8957.392578125)
+    })
+end
+pathfindTable({
+    [1] = Vector3.new(-1031.4885253906, 26.392995834351, 8985.8662109375)
+    [2] = Vector3.new(-989.02899169922, 26.393482208252, 9015.9345703125)
+})
